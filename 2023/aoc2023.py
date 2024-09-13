f4e2237b7585f85f01d20a84c6934424e5405ff0
Zane <zane@mba>
2024-09-13 15:21:44 -0400

    infrastructure; cleanup

--- 2023/aoc2023.py
+++ 2023/aoc2023.py
@@ -5,46 +5,60 @@ import structlog
 from day_one import one_one
 
 # Map string log levels to structlog log level numbers
-log_levels = {
-    'DEBUG': 10,
-    'INFO': 20,
-    'WARNING': 30,
-    'ERROR': 40,
-    'CRITICAL': 50
-}
+log_levels = {"DEBUG": 10, "INFO": 20, "WARNING": 30, "ERROR": 40, "CRITICAL": 50}
 
-commands = {
-    "one": one_one
-}
+commands = {"one": one_one}
+
 
 def get_all_input(f: click.File) -> list[str]:
+    """
+    Get puzzle input from the provided file.
+
+    :param f: click.File from which to read input
+    :returns: list of lines in the provided file
+    """
     return filter(lambda x: x, map(lambda x: x.strip(), f.readlines()))
 
-@click.command()
-@click.argument("day")
-@click.argument("input_file", type=click.File('r'))
-@click.option("--log", type=click.Choice(logging.getLevelNamesMapping().keys()), help="Logging level")
-def main(day, input_file, log):
 
+def configure_logging(preferredLevel: str | None = None, defaultLevel=logging.WARN):
+    """
+    Configure logging; currently structlog.
 
+    :param preferredLevel: preferred logging level; should match one of the values from logging module
+    :param defaultLevel: default logging level if preferredLevel is invalid or missing
+    :returns: nothing
+    """
     # Configure structlog
-    if log: 
-        log_level = logging.getLevelNamesMapping().get(log, logging.WARN)
+    if preferredLevel:
+        log_level = logging.getLevelNamesMapping().get(preferredLevel, defaultLevel)
     else:
-        log_level = logging.INFO
+        log_level = defaultLevel
 
     structlog.configure(
         processors=[
             structlog.processors.add_log_level,
-            structlog.dev.ConsoleRenderer()
+            structlog.dev.ConsoleRenderer(),
         ],
         wrapper_class=structlog.make_filtering_bound_logger(log_level),
     )
+
+
+@click.command()
+@click.argument("day")
+@click.argument("input_file", type=click.File("r"))
+@click.option(
+    "--log",
+    type=click.Choice(logging.getLevelNamesMapping().keys()),
+    help="Logging level",
+)
+def main(day, input_file, log):
+    configure_logging()
 
     puzzle_input = get_all_input(input_file)
     command = commands[day]
     if command is not None:
         command_output = command(puzzle_input)
         print(command_output)
+
 
 main()
--- 2023/day_one.py
+++ 2023/day_one.py
@@ -7,6 +7,7 @@ logger = structlog.get_logger()
 first_num_regex = r"(\d)"
 last_num_regex = r"(\d)(?!.*\d)"
 
+
 def get_calibration_value(line: str) -> int:
     m = re.search(first_num_regex, line)
     if m is not None:
@@ -23,7 +24,13 @@ def get_calibration_value(line: str) -> int:
         raise ValueError("Unable to read last digit.")
 
     result = int(first_digit) * 10 + int(last_digit)
-    logger.debug("line: %s, first: %s, last: %s, result: %d", line, first_digit, last_digit, result)
+    logger.debug(
+        "line: %s, first: %s, last: %s, result: %d",
+        line,
+        first_digit,
+        last_digit,
+        result,
+    )
     return result
 
 
